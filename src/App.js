6ef127ee41da2a1b20f24284841999e70300a2b3
Gaurav <gsaraf895@gmail.com>
2024-05-17 16:12:47 +0530

    error sovedt

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css"
 import Flow from "./Components/BiteSpeedFlow.js";
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 // import "./styles.css";
